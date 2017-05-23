e9cd14bc582998c3bba20ca9f5d66d54c07f70b3
Adrian Martinez <adrianmrit@yahoo.com>
2017-05-23 11:31:30 -0700

    create tables and open an exisitng database

--- smart_drillholes_gui/mainapp/urls.py
+++ smart_drillholes_gui/mainapp/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 import views
 urlpatterns = [
                 url(r'^$', views.index, name='index'),
+                url(r'^open/$', views.open, name='open'),
                 url(r'^new/$', views.new, name='new'),
                 url(r'^dashboard/$', views.dashboard, name='dashboard'),
+                url(r'^add_table/$', views.add_table, name='add_table'),
 ]
--- smart_drillholes_gui/mainapp/views.py
+++ smart_drillholes_gui/mainapp/views.py
@@ -3,32 +3,56 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render, redirect
 from smart_drillholes.core import *
-from .forms import NewForm
+from .forms import OpenForm, NewForm, AddTableForm
 import datetime
 
 # Views
 
 
 def index(request):
-    response =  render(request,
-                  'mainapp/index.html',
-                  {'ref': 'index'})
+    response = render(request,
+                      'mainapp/index.html',
+                      {'ref': 'index'})
     return response
+
+
+def open(request):
+    if request.method == "GET":
+        form = OpenForm()
+        return render(request,
+                      'mainapp/open.html',
+                      {'form': form,
+                       'ref': 'open'})
+    elif request.method == "POST":
+        form = OpenForm(request.POST)
+        if form.is_valid():
+            if form.cleaned_data.get('db_type') == 'sqlite':
+                con_string = 'sqlite:///{}.sqlite'.format(form.cleaned_data.get('name'))
+            elif form.cleaned_data('db_type') == 'postgresql':
+                con_string = 'postgresql://postgres@localhost/{}'.format(form.cleaned_data.get('name'))
+            eng, meta = og_connect(con_string)
+            response = redirect('mainapp:dashboard')
+            expiry_time = datetime.datetime.now() + datetime.timedelta(minutes=525600)
+            response.set_cookie(key='db', value=form.cleaned_data.get('name'), expires=expiry_time)
+            response.set_cookie(key='db_type', value=form.cleaned_data.get('db_type'), expires=expiry_time)
+            return response
+
 
 def new(request):
     if request.method == "GET":
         form = NewForm()
         return render(request,
-                      'mainapp/new.html', {'form': form,
-                                           'ref': 'new'})
+                      'mainapp/new.html',
+                      {'form': form,
+                       'ref': 'new'})
     elif request.method == "POST":
         form = NewForm(request.POST)
         if form.is_valid():
             if form.cleaned_data.get('db_type') == 'sqlite':
-                con_string = 'sqlite:///%s.sqlite' % form.cleaned_data.get('name')
+                con_string = 'sqlite:///{}.sqlite'.format(form.cleaned_data.get('name'))
             elif form.cleaned_data('db_type') == 'postgresql':
-                con_string = 'postgresql://postgres@localhost/%s' % form.cleaned_data.get('name')
-            eng, meta = og_connect(con_string, overwrite=False)
+                con_string = 'postgresql://postgres@localhost/{}'.format(form.cleaned_data.get('name'))
+            eng, meta = og_connect(con_string)
             og_references(eng, meta, table_name='assay_certificate', key='SampleID', cols={'Au': {'coltypes': Float,
                                                                                            'nullable': True}})
             og_references(eng, meta, table_name='rock_catalog', key='RockID', cols={'Description': {'coltypes': String,
@@ -43,13 +67,60 @@ def new(request):
                                                                            'foreignkey': {'column': 'rock_catalog.RockID',
                                                                                           'ondelete': 'RESTRICT',
                                                                                           'onupdate': 'CASCADE'}}})
+            execute(eng, meta)
             response = redirect('mainapp:dashboard')
             expiry_time = datetime.datetime.now() + datetime.timedelta(minutes=525600)
             response.set_cookie(key='db', value=form.cleaned_data.get('name'), expires=expiry_time)
+            response.set_cookie(key='db_type', value=form.cleaned_data.get('db_type'), expires=expiry_time)
             return response
 
+
 def dashboard(request):
-    response =  render(request,
-                  'mainapp/dashboard.html',
-                  {'ref': 'dashboard'})
+    response = render(request,
+                      'mainapp/dashboard.html',
+                      {'ref': 'dashboard'})
     return response
+
+
+def add_table(request):
+    if request.method in ['GET', 'POST']:
+        if request.COOKIES.get('db_type') == "sqlite":
+            con_string = 'sqlite:///{}.sqlite'.format(request.COOKIES.get('db'))
+        elif request.COOKIES.get('db_type') == "postgresql":
+            con_string = 'postgresql://postgres@localhost/{}'.format(request.COOKIES.get('db'))
+        eng, meta = og_connect(con_string)
+    if request.method == 'GET':
+        form = AddTableForm(meta=meta)
+        return render(request,
+                      'mainapp/add_table.html',
+                      {'ref': 'dashboard', 'form': form})
+    elif request.method == 'POST':
+        form = AddTableForm(request.POST, meta=meta)
+        if form.is_valid():
+            if form.cleaned_data.get('table_type') == 'assay_certificate':
+                og_references(eng, meta, table_name=form.cleaned_data.get('name'), key='SampleID', cols={'Au': {'coltypes': Float,
+                                                                                   'nullable': True}})
+            elif form.cleaned_data.get('table_type') == 'rock_catalog':
+                og_references(eng, meta, table_name=form.cleaned_data.get('name'), key='RockID', cols={'Description': {'coltypes': String,
+                                                                                                        'nullable': True}})
+            elif form.cleaned_data.get('table_type') == 'assay':
+                table = form.cleaned_data.get('foreignkey')
+                for column in meta.tables[table].columns:
+                    if column.primary_key:
+                        pk = column.key
+                og_add_interval(eng, meta, table_name=form.cleaned_data.get('name'), cols={'SampleID': {'coltypes': String,
+                                                                                  'nullable': False,
+                                                                                  'foreignkey': {'column': '{}.{}'.format(table, pk),
+                                                                                                 'ondelete': 'RESTRICT',
+                                                                                                 'onupdate': 'CASCADE'}}})
+            elif form.cleaned_data.get('table_type') == 'litho':
+                for column in meta.tables[table].columns:
+                    if column.primary_key:
+                        pk = column.key
+                og_add_interval(eng, meta, table_name=form.cleaned_data.get('name'), cols={'RockID':{'coltypes': String,
+                                                                               'nullable': True,
+                                                                               'foreignkey': {'column': '{}.{}'.format(table, pk),
+                                                                                              'ondelete': 'RESTRICT',
+                                                                                              'onupdate': 'CASCADE'}}})
+            execute(eng, meta)
+        return redirect('mainapp:dashboard')
